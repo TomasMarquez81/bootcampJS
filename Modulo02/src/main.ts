ac63dbd75d65f7d0b50d56f704a6b5d243ebd7f9
TomasMarquez81 <johnniew_@hotmail.com>
2025-06-03 15:10:09 +0200

    Modulo02

---
+++ Modulo02/src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+let total: number = 120; 
+let friends: number = 6;
+let drink: number = 18
+
+console.log(`¡Por mi cumple invito a las bebidas ${drink} €! 🍻 La comida sale ${(total - drink) / friends} € por persona, que el bolsillo no da para todo, pero las ganas de celebrarlo con vosotros sí 😄` );
